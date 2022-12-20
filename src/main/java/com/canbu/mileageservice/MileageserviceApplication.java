4f6fdd88fd1e25a29a6296971ecbb3f5c5f41215
dlalscjf94 <ssw01092@naver.com>
2022-12-20 22:55:48 +0900

    build : init project

---
+++ src/main/java/com/canbu/mileageservice/MileageserviceApplication.java
@@ -0,0 +1,13 @@
+package com.canbu.mileageservice;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MileageserviceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MileageserviceApplication.class, args);
+    }
+}
---
+++ src/test/java/com/canbu/mileageservice/MileageserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.canbu.mileageservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MileageserviceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
